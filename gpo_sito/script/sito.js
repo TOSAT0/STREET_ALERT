4182cc712a83bd4a756f23f42d7d7ef74c1493ca
TOSAT0 <micheletosato05@gmail.com>
2024-05-03 18:48:24 +0200

    cose

--- gpo_sito/script/sito.js
+++ gpo_sito/script/sito.js
@@ -1,38 +1,7 @@
+var table = document.getElementById("table")
 
-
-let xhttp = new XMLHttpRequest()
-
-var id_user = "1"
-
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log(this.response)
-        res = JSON.parse(this.response)
-
-        switch(res.status){
-        	case "success-old_lists":
-                contents = res.contents
-                print_lists()
-                break
-            case "success-update_list":
-                console.log("update")
-                break
-            case "success-new_list":
-                console.log("insert")
-                break
-            
-            case "error-old_lists":
-                old_contents.innerHTML = "error 404"
-                break
-            case "error-update_list":
-                old_contents.innerHTML = "error 404"
-                break
-            case "error-new_list":
-                old_contents.innerHTML = "error 404"
-                break
-            
-            default:
-                alert(xhttp.responseText)
-        }
+window.onload = function(){
+	if (localStorage.getItem('id')){
+        table.innerHTML = "<a href=\"table/table.html\" class=\"hover-link\">Lorem</a>"
     }
 }
--- gpo_sito/server/sito.php
+++ gpo_sito/server/sito.php
@@ -1,3 +1,20 @@
 <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $status = $_POST["status"];
 
+        switch($status){
+            case "login":
+                $contents = get_contents($_POST["id_list"]);
+                $response = array("status" => $contents ? "success-old_contents" : "error-old_contents", "contents" => $contents);
+                break;
+            case "signup":
+                $status = update_content($_POST["id_content"], $_POST["content_name"], $_POST["check_view"]);
+                $response = array("status" => $status ? "success-update_content" : "error-update_content");
+                break;
+        }
+
+        header('Content-Type: application/json');
+        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+        echo json_encode($response);
+    }
 ?>
