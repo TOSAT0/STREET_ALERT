e5c0bba70cf1b5111d6090f49f74f363efe11f78
damianogobbo <cagnin.st.andrea@maxplanck.edu.it>
2024-05-09 12:51:45 +0200

    image path changed and account created

---
+++ account/js/index.js
@@ -0,0 +1,12 @@
+// let xhttp = new XMLHttpRequest();
+
+// xhttp.onreadystatechange = function()
+// {
+//     if (this.readyState == 4 && this.status == 200) {
+//         setData(JSON.parse(this.response))
+//         showMarkers()
+//     }
+// }
+
+// xhttp.open("POST","server.php", true)
+// xhttp.send()
---
+++ account/php/server.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "127.0.0.1";
+$user = "root";
+$pw = "";
+$db = "my_streetalert";
+
+
+
+// $conn = new mysqli($host, $user, $pw, $db);
+
+// $query = "SELECT * FROM alerts";
+
+// return $conn -> query($query);
