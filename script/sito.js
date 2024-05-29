5cff2a7919b7f396916fa015013d32ba99cea219
TOSAT0 <micheletosato05@gmail.com>
2024-05-29 10:00:57 +0200

    zak gay

--- script/sito.js
+++ script/sito.js
@@ -1,8 +1,10 @@
 var table = document.getElementById("table")
+var login = document.getElementById("login")
 
 window.onload = function(){
-    localStorage.setItem('id',0)
-	if (localStorage.getItem('id')){
-        table.innerHTML = "<a href=\"table/table.html\" class=\"hover-link\">Area personale</a>"
-    }
+    localStorage.clear()
+	if (localStorage.getItem('id'))
+        table.innerHTML = "<a href=\"table/table.html\" class=\"hover-link secondary-button\">Area personale</a>"
+    else
+        login.innerHTML = "<a href=\"login/login.html\" class=\"hover-link secondary-button\">Accedi</a>"
 }
