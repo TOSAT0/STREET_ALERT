1b34ed0ae18116ea829386666697575e73bed8d4
zakpupo <berrad.st.zakaria@maxplanck.edu.it>
2024-06-03 19:48:59 +0200

    Completato sito
    Ho tolto tutte le robe vecchie che sinceramente erano inguardabili e ho riempito anche se di poco il sito

--- script/sito.js
+++ script/sito.js
@@ -7,3 +7,16 @@ window.onload = function(){
     else
         login.innerHTML = "<a href=\"login/login.html\" class=\"hover-link secondary-button\">Area riservata al comune</a>"
 }
+
+const observer = new IntersectionObserver ((entries) => { entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+    } else {
+        entry.target.classList.remove('show');
+    }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
